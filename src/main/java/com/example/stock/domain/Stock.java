df7f00816134915bad51489d674892f09dd4bcdc
baejunil <bjo6300@naver.com>
2024-10-06 21:59:38 +0900

    feat : 재고 감소 기본 로직 구현

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,38 @@
+package com.example.stock.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    @Version
+    private Long version;
+
+    public Stock() {
+
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("재고는 0개 미만이 될 수 없습니다.");
+        }
+
+        this.quantity -= quantity;
+    }
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+}
