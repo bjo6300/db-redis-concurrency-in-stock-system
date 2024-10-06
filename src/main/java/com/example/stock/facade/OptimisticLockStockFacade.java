1aa4ba50763927b59f127f094f12b8dde7a4d4d7
baejunil <bjo6300@naver.com>
2024-10-06 22:48:58 +0900

    feat : 낙관적 락 - 100개 동시 감소 처리

---
+++ src/main/java/com/example/stock/facade/OptimisticLockStockFacade.java
@@ -0,0 +1,28 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.OptimisticLockStockService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OptimisticLockStockFacade {
+    // 실패 시 재시도를 위한 Facade
+
+    private OptimisticLockStockService optimisticLockStockService;
+
+    public OptimisticLockStockFacade(OptimisticLockStockService optimisticLockStockService) {
+        this.optimisticLockStockService = optimisticLockStockService;
+    }
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+
+        while (true) {
+            try {
+                optimisticLockStockService.decrease(id, quantity);
+
+                break;
+            } catch (Exception e) {
+                Thread.sleep(50);
+            }
+        }
+    }
+}
--- src/main/java/com/example/stock/repository/StockRepository.java
+++ src/main/java/com/example/stock/repository/StockRepository.java
@@ -11,4 +11,8 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE) // 비관적 락
     @Query("select s from Stock s where s.id = :id")
     Stock findByIdWithPessimisticLock(Long id);
+
+    @Lock(LockModeType.OPTIMISTIC) // 낙관적 락 - Stock에 version 추가
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithOptimisticLock(Long id);
 }
