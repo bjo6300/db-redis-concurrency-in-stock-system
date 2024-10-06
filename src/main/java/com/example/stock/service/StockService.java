1f1d6dd5acb9d3a42244453632b14d8fe1077739
baejunil <bjo6300@naver.com>
2024-10-06 22:28:38 +0900

    feat : synchronized - 100개 동시 감소 처리

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -15,8 +15,10 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void decrease(Long id, Long quantity) {
+    // @Transactional(propagation = Propagation.REQUIRES_NEW)
+    // Transaction을 사용하면 decrease가 완료되고 트랜잭션이 종료되어 db에 반영하기 전에 다른 쓰레드가 quantity를 가져가는 상황이 발생해 재고 수량이 맞지 않는다.
+    // 한 프로세스 안에서만 동시성이 보장된다. -> 서버가 여러 대인 경우 동시성을 보장하지 못한다.
+    public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
 
         stock.decrease(quantity);
